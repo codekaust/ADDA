a0e8228b0d4c2ee64bd787c6e67495ebe06bae8d
codekaust <kaustubh1303@gmail.com>
2018-01-11 01:41:32 +0530

    updated

--- app/src/main/java/com/example/android/adda/DecisionActivity.java
+++ app/src/main/java/com/example/android/adda/DecisionActivity.java
@@ -46,12 +46,10 @@ public class DecisionActivity extends AppCompatActivity {
 
                     startActivity(new Intent(DecisionActivity.this,SetUpUsernameActivity.class));
                     progressDialog.dismiss();
-                    finish();
                 }
                 else {
                     startActivity(new Intent(DecisionActivity.this,HomeActivity.class));
                     progressDialog.dismiss();
-                    finish();
                 }
             }
 
